d09ae162035c8121cdcbf366a6c7d1a474791d61
BASTEEN SANTHIYAGU A <santhiyagubasteen@gmail.com>
2025-04-13 19:12:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const plantRouter = require('./routes/plant');
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Mount existing routers
-// app.use('/api/plant', plantRouter);
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/fields', require('./routes/fields'));
 app.use('/api/bills', require('./routes/bills'));
 
-// Mount the new IoT router
+
 app.use('/api/iot', require('./routes/iot'));
 
 const PORT = process.env.PORT || 5000;
